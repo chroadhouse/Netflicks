4a6e48bb838199adda37d8da8892cdbe49cf05c1
chroadhouse <ch.roadhouse@gmail.com>
2021-03-23 16:48:14 +0000

    header and footer changed for all

--- include/footer.php
+++ include/footer.php
@@ -3,29 +3,29 @@
 	<div class = container5> <!-- Containers used for redirections to information about the site -->
 		<header2> <b> BASICS </b> </header2>
 		<br> </br>
-			<button3> <u> Support </u> </button3>
+			<button3> <a href="Basics.php"><u> Support </u></a> </button3>
 		<br> </br>
-			<button3> <u> Account Usage </u> </button3>
+			<button3><a href="Basics.php"><u> Account Usage </u> </a></button3>
 	</div>
 
 	<div class = container6>
 		<header3> <b>  ABOUT US </b> </header3>
 		<br> </br>
-			<button3> <u> Developers </u> </button3> 
+			<button3> <a href="AboutUs.php"><u> Developers </a></u> </button3> 
 			<br> </br>
-			<button3> <u> Email </u> </button3> 
+			<button3> <a href="AboutUs.php"><u> Email </u> </a></button3> 
 			<br> </br>
-			<button3> <u> Contact Us </u> </button3>
+			<button3> <a href="AboutUs.php"><u> Contact Us </u> </a></button3>
 	</div>
 
 	<div class = container7>
 		<header4> <b> LEGALS </b> </header4>
 		<br> </br>
-			<button3> <u> Health & Safety </u> </button3>
+			<button3> <a href="Legals.php"><u> Health & Safety </u></a> </button3>
 			<br> </br>
-			<button3> <u> Privacy </u> </button3>
+			<button3><a href="Legals.php"> <u> Privacy </u></a> </button3>
 			<br> </br>
-			<button3> <u> Conditions of Use </u> </button3>
+			<button3><a href="Legals.php"> <u> Conditions of Use </u></a> </button3>
 	</div>
 </div>
 </footer>
