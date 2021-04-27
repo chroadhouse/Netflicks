4db2cfe1b9c83bb4574e7b8a815c810d442e71b6
wayne582 <80901911+wayne582@users.noreply.github.com>
2021-04-27 16:07:10 +0100

    Update behaviour.js

--- js/behaviour.js
+++ js/behaviour.js
@@ -1,4 +1,6 @@
 	//wait for html page to be fully loaded and register custom listners 
+	//Page will initilise for either login page to open or the sign up page to open once the user has clicked on the event to occur
+	//This will call each of the function called redirecttoURL and the redirectToSignUpUrl to open the html files accordingly
 	document.addEventListener("DOMContentLoaded",  initialiseWebPage);
 
 	function initialiseWebPage() {
